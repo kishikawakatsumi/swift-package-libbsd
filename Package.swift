87fd7f7a7610c87a12a330c2ae823904996e86d7
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2018-04-08 09:12:09 +0900

    Initial import

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BSD",
+    providers: [
+        .apt(["libbsd-dev"])
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-package-libbsd
+
+Simple wrapper around the C Library BSD for use with Swift Package Manager on Linux
